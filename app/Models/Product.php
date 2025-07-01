c296a27732b16c87c8192b8c33ce05a8274ed2f5
Motherbloods <habibskh06@gmail.com>
2025-07-01 19:35:17 +0700

    feat: add Product model and migration

---
+++ app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'section',
+        'category',
+        'price',
+        'description',
+        'image',
+        'stock',
+    ];
+
+    protected static function booted()
+    {
+        static::creating(function ($product) {
+            if (empty($product->slug)) {
+                $product->slug = Str::slug($product->name);
+            }
+        });
+    }
+}
